de6ff7191e40c8f69ebd63bddd7784941264c5ac
FeriPratama <117093475+CyberCarboon2@users.noreply.github.com>
2023-01-25 22:35:15 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Mau Recode Yaaaa???
+# Silahkan Bree Izin Dulu Tapi Lah
+# Feri Pratama X Rafly
+# CyberCarboon2
+import os,sys
+import Fera
+
+def __Feri__X__Rafly__():
+	Fera.main()
+
+if __name__=="__main__":
+	__Feri__X__Rafly__()
