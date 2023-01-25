34e6fd2b6d80ff906029980dd283b1099f4a49d6
FeriPratama <117093475+CyberCarboon2@users.noreply.github.com>
2023-01-25 22:46:49 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Mau Recode Yaaaa???
+# Silahkan Bree Izin Dulu Tapi Lah
+# Feri Pratama X Rafly
+# CyberCarboon2
+from os import sys as function
+import Fera
+def __Fera__():
+	function('python Fera.py')
+def __Feri__X__Rafly__():
+	try:
+		Fera.main()
+	except AttributeError:
+		__Fera__()
+if __name__=="__main__":
+	__Feri__X__Rafly__()
